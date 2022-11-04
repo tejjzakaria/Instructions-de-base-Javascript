5221ea796cd13c4cba8e377c94b82ad209844b73
tejjzakaria <zakaria.tejjani@gmail.com>
2022-11-04 17:34:46 +0100

    2nd commit

--- Script_Programme_base001.js
+++ Script_Programme_base001.js
@@ -4,8 +4,14 @@
 var a,b,som,prod,soust,div;  //order envoyee a la memoire pour faire une reservation
 // les valeurs 12 et 7.5 sont comme des constantes
 
-a=12;
-b=7.5;
+//a=12;
+//b=57;
+
+// les valeurs doivent etre variables pour l'utilisateur
+
+a=parseFloat(prompt("Entrez la valeur de a :"));
+b=parseFloat(prompt("Entrez la valeur de b :"));
+
 
 // donner l'ordre au cpu au cervau electronique a travers les uals (unite arithmetique et logique)
 
@@ -21,7 +27,7 @@ console.log("b :"+" "+b);
 console.log("som :"+" "+som);
 console.log("prod :"+" "+prod);
 console.log("soust :"+" "+soust);
-console.log("div :"+" "+div);
+console.log("div :"+" "+div.toPrecision(2));
 
 //Bitage des variables declarees ou reservees dans la memoire
 
@@ -48,4 +54,26 @@ document.writeln("b = "+" "+b+'<br><br>');
 document.writeln("som = "+" "+som+'<br><br>');
 document.writeln("prod = "+" "+prod+'<br><br>');
 document.writeln("soust = "+" "+soust+'<br><br>');
-document.writeln("div = "+" "+div+'<br><br>');
+document.writeln("div = "+" "+div.toPrecision(2));
+
+// hexadecimal
+
+//Bitage des variables declarees ou reservees dans la memoire hexadecimal
+
+console.log("bitage de a en hexadecimal :"+" "+a.toString(16));
+console.log("bitage de b en hexadecimal :"+" "+b.toString(16));
+console.log("bitage de som en hexadecimal :"+" "+som.toString(16));
+console.log("probitage de prod en hexadecimal :"+" "+prod.toString(16));
+console.log("bitage de soust en hexadecimal :"+" "+soust.toString(16));
+console.log("bitage de div en hexadecimal :"+" "+div.toString(16));
+
+// octal
+
+//Bitage des variables declarees ou reservees dans la memoire octal
+
+console.log("bitage de a en octal :"+" "+a.toString(8));
+console.log("bitage de b en octal :"+" "+b.toString(8));
+console.log("bitage de som en octal :"+" "+som.toString(8));
+console.log("probitage de prod en octal :"+" "+prod.toString(8));
+console.log("bitage de soust en octal :"+" "+soust.toString(8));
+console.log("bitage de div en octal :"+" "+div.toString(8));
